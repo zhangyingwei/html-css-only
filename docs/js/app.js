67ad60fd6998c18159749fc9bba4aa6e3daf22d4
zhangyingwei <zhangyw_001@126.com>
2020-03-17 13:27:55 +0800

    docs

---
+++ docs/js/app.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    projects:[
+      {
+        name: "2020 你今天又进步了吗",
+        path: "/2020"
+      },{
+        name: "原来 css 可以这么漂亮",
+        path: "/login"
+      },{
+        name: "不用 JS 也可以做一个带动画的输入框",
+        path: "/button"
+      },{
+        name: "抖音，记录美好生活！",
+        path: "/douyin"
+      },{
+        name: "整个CSS动画，祝大家新年快乐！！！",
+        path: "/jianzhi"
+      },{
+        name: "用一段普通的CSS，整一个不普通的按钮效果",
+        path: "/border-button"
+      },{
+        name: "CSS 加载动画系列",
+        path: "/loading"
+      }
+    ]
+  }
+})
