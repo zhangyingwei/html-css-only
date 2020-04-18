5f0fe65497130eae249688554277d13b25ee5170
zhangyingwei <zhangyw_001@126.com>
2020-04-18 12:40:49 +0800

    生成images

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,32 +3,21 @@ var app = new Vue({
   data: {
     projects:[
       {
-        name: "2020 你今天又进步了吗",
-        path: "2020"
-      },{
-        name: "原来 css 可以这么漂亮",
-        path: "login"
-      },{
-        name: "不用 JS 也可以做一个带动画的输入框",
-        path: "button"
-      },{
-        name: "抖音，记录美好生活！",
-        path: "douyin"
-      },{
-        name: "整个CSS动画，祝大家新年快乐！！！",
-        path: "jianzhi"
-      },{
-        name: "用一段普通的CSS，整一个不普通的按钮效果",
-        path: "border-button"
-      },{
-        name: "CSS 加载动画系列",
-        path: "loading"
+        name: "2020",
+        video: "https://www.bilibili.com/video/BV1xJ411G7ar/"
+      },
+      {
+        name: "card",
+        video: "https://www.bilibili.com/video/BV1bJ411s7rj/"
       }
     ]
   },
   methods: {
     getImagePath(pathName){
       return "https://raw.githubusercontent.com/zhangyingwei/html-css-only/master/"+pathName+"/image/profile.png"
+    },
+    getPagePath(pathName){
+      return "http://htmlpreview.github.io/?https://github.com/zhangyingwei/html-css-only/blob/master/"+pathName+"/index.html"
     }
   }
 })
